a68150418cbba7c959f50573888fbcb4e4aaf65a
MDTrue <MDTrueII@hotmail.com>
2019-01-31 00:13:49 -0800

    set up folders for SASS

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import OwnerSignup from "./pages/OwnerSignup";
-import Navbar from "./components/Navbar";
-import { Container } from "../components/Grid";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
+// import OwnerSignup from "./pages/OwnerSignup";
+import Header from "./components/Header";
+import { Container } from "./components/Grid";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <Header />
         <Container>
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} />
           <Route exact path="/user/login" component={Login} />
           <Route exact path="/walker/signup" component={Signup} />
-          <Route exact path="/owner/signup" component={OwnerSignup} />
+          <Route exact path="/owner/signup" component={OwnerSignup} /> */}
         </Container>
         <Footer />
       </div>
