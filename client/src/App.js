1de6030fcdba3eab8f1fd1e9f7d136860a4bbccc
MDTrue <MDTrueII@hotmail.com>
2019-02-01 00:19:15 -0800

    saving work on webpages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import splash from "./pages/splash";
 //import Home from "./pages/Home";
 //import Login from "./pages/Login";
 //import Signup from "./pages/Signup";
 //import OwnerSignup from "./pages/OwnerSignup";
 import Navbar from "./components/Navbar";
-
-import { Container } from "./components/Grid";
+import Container from "./components/Container"
+import Wrapper from "./components/Wrapper";
+// import { Container } from "./components/Grid";
 import Footer from "./components/Footer";
 import TodayWalks from "./components/TodayWalks";
 
@@ -16,17 +18,17 @@ function App() {
   return (
     <Router>
       <div>
-        <Header />
-        <Container>
-
+        {/* <Header /> */}
+        <Wrapper>
+           <Route exact path="/splash" component={splash} />
          {/* <Route exact path="/" component={Home} />*/}
         { /* <Route exact path="/user/login" component={Login} /> */} 
         { /*  <Route exact path="/walker/signup" component={Signup} />*/} 
         { /*  <Route exact path="/owner/signup" component={OwnerSignup} />*/} 
           <Route exact path="/walker/todayWalks" component={TodayWalks} />
           
-        </Container>
-        <Footer />
+        </Wrapper>
+        {/* <Footer /> */}
       </div>
     </Router>
   );
--- client/src/pages/splash.js
+++ client/src/pages/splash.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {Component } from 'react';
 //import ReactDOM from 'react-dom';
 import "../index.css";
+// import Container from "../components/Container";
+import { Col, Row, Container } from "../components/Grid";
+// import Footer from "../components/Footer";
+import logo from "../images/tailMeLogo.png";
 
 
 class splash extends Component {
     render() {
+        return (
         <Container >
-            <Footer />
+            <div className="splash">
+            <img className="splash__logo" src={logo}alt="tailME logo" ></img>
+           
+            </div>
         </Container>
+        )
     }
 }
 export default splash
