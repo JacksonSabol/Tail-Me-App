10a0ca8811242dd2cc2038514f543cee4a5e8d80
Isabel <iarcones@gmail.com>
2019-02-04 21:29:19 -0800

    Implemented API call createOwner conected to the Walker and destroy the invitationPending table

---
+++ client/src/components/ShowPicsMap/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+ 
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.7912398,
+      lng: -122.39435999999998
+    },
+    zoom: 11
+  };
+ 
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.7912398}
+            lng={-122.39435999999998}
+            text={'Walk-1'}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+ 
+export default SimpleMap;
--- controllers/globalController.js
+++ controllers/globalController.js
@@ -2,6 +2,8 @@ const db = require("../models");
 
 module.exports = {
 
+    // need to clean the code to make one API call for twilio with invitation body and phoneTO, to be called from any other function.. This fuction will be called from: server reminders, createInvitation, alert walker a customer has signed up,...
+
     createInvitation: function (req, res) {
 
         console.log("params...: ", req.params)
@@ -15,11 +17,11 @@ module.exports = {
                 walkerId: req.params.walkerid       /// this code need to come with the state
             })
             // .then(dbModel => console.log(dbModel))
-            .then(dbModel => sendSMS(dbModel, walkerName))
+            .then(dbModel => sendSMSowner(dbModel, walkerName))
             .catch(err => res.status(422).json(err));
 
 
-        function sendSMS(dbModel, walkerName) {
+        function sendSMSowner(dbModel, walkerName) {
        
             require('dotenv').config();
             var id = process.env.TWILIO_ACCOUNT_SID;
@@ -32,13 +34,14 @@ module.exports = {
        
             var ownerName = dbModel.name;
             var phoneNumber = dbModel.phoneNumber;
-            var urlCode = dbModel.specialCode;
+            var urlCode1 = dbModel.walkeId;
+            var urlCode2 = dbModel.specialCode;
             // var walkerName = req.cookie.name;
             var walkerName = walkerName;
 
             //I need to include in the invitation the URL with a ref with the Walker ID + the owner phonenumber and a specific login path
 
-            var invitation = `Hi ${ownerName}, ${walkerName} has invited you to join TailMe and setup all the info about your dog. http://localhost:3000/owner/signup/${urlCode}`
+            var invitation = `Hi ${ownerName}, ${walkerName} has invited you to join TailMe and setup all the info about your dog. http://localhost:3000/owner/signup/${urlCode1}/${urlCode2}`
        
             console.log("invitation: ", invitation);
 
@@ -46,6 +49,72 @@ module.exports = {
                 .create({
                     body: invitation,
                     from: phonefrom,
+                    to: phoneNumber
+                })
+                .then(message => {
+                    console.log(message.sid);
+                    res.json(message);
+                    //create a row in the  invitations pending table
+                    //res.json(res)
+                })
+                .done(console.log("sended"));
+        }
+    },
+
+    createOwner: function (req, res) {
+        //inputs: userId (owner) + specialcode: if scpecialcode = 0 (create dogowner), if !0 create dogowner + clean the pendinginvitatios + send sms to walker telling the owner signup
+
+        console.log("params...: ", req.params)
+        var ownerUserId = req.params.owneruserid;  // this is coming from the user table created during the signup process
+        var specialCode = req.params.specialcode;  // this is coming from the URL signup route
+        var walkerId = req.params.walkerid;        // this is coming from the URL signup route
+
+        db.dogOwner
+            .create({
+                userId: ownerUserId,
+                walkerId: walkerId,
+                emergencyContact: `+1${specialCode}`
+            })
+            // .then(dbModel => console.log(dbModel))
+            .then(dbModel =>{
+                db.invitationPending
+                .destroy({where: {
+                    specialCode: specialCode
+                  }
+                })
+                .then(dbModel => console.log(dbModel))
+                // .then(dbModel => sendSMSwalker(data))
+                .catch(err => res.status(422).json(err))
+            })
+            .catch(err => res.status(422).json(err));
+
+      
+
+            /// this will be implemented if the Walker has phone number in the user table or in the walker table to informed the person that a customer has signup
+        function sendSMSwalker(data) {
+       
+            require('dotenv').config();
+            var id = process.env.TWILIO_ACCOUNT_SID;
+            var secret = process.env.TWILIO_TOKEN;
+            var phonefrom = process.env.TWILIO_PHONE_FROM;
+
+            const client = require('twilio')(id, secret);
+
+            // route to friends SQL table here
+       
+            var ownerName = data.ownerName;
+            var walkerPhoneNumber = data.walkerPhoneNumber;
+            // var walkerName = req.cookie.name;
+            var walkerName = data.walkerName;
+
+            var invitation = `Hi ${walkerName}, your customer: ${ownerName}, has finished the sign up process`
+       
+            console.log("invitation: ", invitation);
+
+            client.messages
+                .create({
+                    body: invitation,
+                    from: walkerPhoneNumber,
                     to: phoneNumber
                 })
                 .then(message => {
