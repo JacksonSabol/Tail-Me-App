a0a1024fb2e814d31090c579a0866861a13b235f
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2019-02-06 17:49:27 -0800

    add functionality for owners to add new dogs; add redirects to components when api calls are complete; split profile into owner and walker view depending on who is logged in

--- routes/api/dogRoutes.js
+++ routes/api/dogRoutes.js
@@ -4,17 +4,19 @@ const dogController = require("../../controllers/dogController");
 // Matches with "/api/user
 router.route("/:id")
   .get(dogController.findOne)
- // .delete(userController.remove);
+// .delete(userController.remove);
 
-
+// Matches with "/api/dogProfile/findWalker"
+router.route("/findWalker")
+  .get(dogController.findWalker)
 
 // Matches with "/api/dogProfile/create"
 router.route("/create")
   .post(dogController.create);
 
-  // Matches with "/api/"
+// Matches with "/api/"
 router.route("/:idOwner/gallery")
-.get(dogController.getImagesOwner);
+  .get(dogController.getImagesOwner);
 
 
 module.exports = router;
