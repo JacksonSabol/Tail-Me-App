448b53143ea591dc8be3d8289ba86de1ff46238e
MDTrue <MDTrueII@hotmail.com>
2019-02-28 10:56:56 -0800

    a few updates

--- client/src/components/TopBarNavWalker/index.js
+++ client/src/components/TopBarNavWalker/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import logo from "../../images/tailMeLogo.png";
+import certificationIcon from "../../images/certificationIcon.png";
+import imagesIcon from"../../images/imagesIcon.png";
+import inviteIcon from "../../images/inviteIcon.png";
+import logoutIcon from "../../images/logoutIcon.png";
+import mapIcon from "../../images/mapIcon.png";
+import profileIcon from "../../images/profileIcon.png";
+import scheduleIcon from "../../images/scheduleIcon.png";
+import walkIcon from "../../images/walkIcon.png";
 import "../../index.css";
 
 // Depending on the current path, make props set an "active" class on the appropriate navigation link item
@@ -7,7 +15,9 @@ import "../../index.css";
 function TopBarNavWalker(props) {
     return (
         <div className="topBarWalker">
-            <a href="/" className="topBarWalker__brand"><img src={logo} className="topBarWalker__brand--logo" alt="^_^" /></a>
+            <a href="/" className="topBarWalker__brand"><img src={logo} className="topBarWalker__brand--logo" alt="^_^" />
+            <span className="topBarWalker__brand--logotooltip">Home</span>
+            </a>
             <p className="topBarWalker__title">Welcome, {props.username}!</p>
             <div className="topBarWalker__nav">
                 <div className="topBarWalker__nav--itemDashboard">
@@ -15,7 +25,7 @@ function TopBarNavWalker(props) {
                         href="#home"
                         onClick={() => props.handlePageChange("Home")}
                     >
-                        Profile
+                        <img className="topBarWalker__nav--profile" src={profileIcon} alt="profile icon" ></img>
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemWalks">
@@ -23,7 +33,8 @@ function TopBarNavWalker(props) {
                         href="#walks"
                         onClick={() => props.handlePageChange("Walks")}
                     >
-                        Upcoming Walks
+                     <img className="topBarWalker__nav--walks" src={walkIcon} alt="walk icon" ></img>
+                        {/* Upcoming Walks */}
                     </a>
                 </div>
                 {/* <div className="topBarWalker__nav--itemSchedWalks">
@@ -39,7 +50,8 @@ function TopBarNavWalker(props) {
                         href="#fullschedule"
                         onClick={() => props.handlePageChange("FullSchedule")}
                     >
-                        Full Schedule
+                     <img className="topBarWalker__nav--schedule" src={scheduleIcon} alt="scheduleicon" ></img>
+                        {/* Full Schedule */}
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemFullSchedule">
@@ -47,7 +59,8 @@ function TopBarNavWalker(props) {
                         href="#invite"
                         onClick={() => props.handlePageChange("Invite")}
                     >
-                        Invite Clients
+                     <img className="topBarWalker__nav--invite" src={inviteIcon} alt="invite icon" ></img>
+                        {/* Invite Clients */}
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemInvite">
@@ -55,7 +68,8 @@ function TopBarNavWalker(props) {
                         href="#certs"
                         onClick={() => props.handlePageChange("Certs")}
                     >
-                        Update Qualifications
+                     <img className="topBarWalker__nav--certification" src={certificationIcon} alt="certification icon" ></img>
+                        {/* Update Qualifications */}
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemCerts">
@@ -63,7 +77,8 @@ function TopBarNavWalker(props) {
                         href="#map"
                         onClick={() => props.handlePageChange("Map")}
                     >
-                        Map
+                     <img className="topBarWalker__nav--map" src={mapIcon} alt="map icon" ></img>
+                        {/* Map */}
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemMap">
@@ -71,7 +86,8 @@ function TopBarNavWalker(props) {
                         href="#upload"
                         onClick={() => props.handlePageChange("Upload")}
                     >
-                        Upload Images
+                     <img className="topBarWalker__nav--images" src={imagesIcon} alt="images icon" ></img>
+                        {/* Upload Images */}
                     </a>
                 </div>
                 <div className="topBarWalker__nav--itemNotifications">
@@ -79,7 +95,8 @@ function TopBarNavWalker(props) {
                         href="#"
                         onClick={() => props.handleLogOut()}
                     >
-                        Log Out
+                     <img className="topBarWalker__nav--logout" src={logoutIcon} alt="logout icon" ></img>
+                        {/* Log Out */}
                     </a>
                 </div>
                 {/* <div className="topBarWalker__nav--item">
