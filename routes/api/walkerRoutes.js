754951a96ba8dcfb57e0ce3a0a37611da79c935a
Isabel <iarcones@gmail.com>
2019-03-11 15:25:24 -0700

    included upload profile picture

--- routes/api/walkerRoutes.js
+++ routes/api/walkerRoutes.js
@@ -98,4 +98,8 @@ router.route("/updatepath/:walkId/:lat/:lng")
 router.route("/getpath/:walkId")
 .get(walkerController.getPath);
 
+router.route("/uploadProfilePicture/:userId")
+  .put(walkerController.uploadProfilePicture);
+
+ 
 module.exports = router;
